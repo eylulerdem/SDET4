b037ac559565c2695d15f5335fef01f3d4ace0ec
eylul <eylulerdem.3019@gmail.com>
2022-11-03 11:13:12 +0300

    günler düzemlendi

--- src/Gun08/Ogrenme.java
+++
@@ -1,7 +0,0 @@
-package Gun08;
-
-public class Ogrenme {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Gun09/_03_ActionsGiris.java
@@ -0,0 +1,15 @@
+package Gun09;
+
+public class _03_ActionsGiris {
+ //   Interview Soruları :
+   // Action Class nedir ? nerelerde kullanılır ? neler yapabilirsiniz ?
+   /*
+   WebElement lerde kullanılır.         Hoverover, doubleClick, rightClick, drag and drop,
+    özel tuşlara basma (shtift,ctrl, yön tuşları)
+
+    Action Sınıfnı nasıl kullanırsın ?
+    Actions aksiyonlar=new Actions(driver); // driver üzerinden aksiyonları kullanacağız.
+    */
+
+
+}
